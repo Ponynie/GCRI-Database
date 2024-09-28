a9eb9c373d5b1c52fddacefbd8a7ccbb05e1f520
Phichaphop Raqsaboon <105978782+Ponynie@users.noreply.github.com>
2024-09-29 03:54:38 +0700

    change 2D column name

--- src/main.py
+++ src/main.py
@@ -7,10 +7,12 @@ output_files = ["data/data-GCxGC_train.csv",
                 "data/data-GCxGC_test.csv", 
                 "data/data-GCxGC_validation.csv"]
 
-# Define the columns to keep
+# Define the columns to keep and the new column names
 columns_to_keep = ["SMILES", "Alkane RI", "PEG-2I (values for compounds with -999 could not be calculated)"]
+new_column_names = {"SMILES": "smiles", "Alkane RI": "ri", 
+                    "PEG-2I (values for compounds with -999 could not be calculated)": "ri_2"}
 
-# Load each sheet, filter the columns, and remove rows where PEG-2I equals -999, then save to CSV
+# Load each sheet, filter the columns, rename them, remove rows where PEG-2I equals -999, then save to CSV
 for sheet, output_file in zip(sheets, output_files):
     # Load the sheet
     df = pd.read_excel(file_path, sheet_name=sheet)
@@ -18,10 +20,13 @@ for sheet, output_file in zip(sheets, output_files):
     # Filter the columns
     df_filtered = df[columns_to_keep]
     
-    # Remove rows where 'PEG-2I' equals -999
-    df_filtered = df_filtered[df_filtered[columns_to_keep[2]] != -999]
+    # Rename columns
+    df_filtered = df_filtered.rename(columns=new_column_names)
+    
+    # Remove rows where 'ri_2' equals -999
+    df_filtered = df_filtered[df_filtered["ri_2"] != -999]
     
     # Save to CSV
     df_filtered.to_csv(output_file, index=False)
 
-print("CSV files created successfully!")
+print("CSV files created and columns renamed successfully!")
